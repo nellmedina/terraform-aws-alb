23bfc6df4f9f44c9525d899d58a31de28aae7ac1
Marinell Medina <marinellmedina1@gmail.com>
2020-03-25 01:05:20 +0800

    update

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,21 +1,27 @@
+variable "alb_name" {
+  default = "lb1"
+}
+
 module "alb" {
   source             = "../../"
-  name               = "example"
-  vpc_id             = "${module.vpc.vpc_id}"
-  subnets            = module.vpc.public_subnet_ids
-  access_logs_bucket = "${module.s3_lb_log.s3_bucket_id}"
-  certificate_arn    = "${module.certificate.acm_certificate_arn}"
+  name               = var.alb_name
+  # vpc_id             = module.vpc.vpc_id
+  # subnets            = module.vpc.public_subnet_ids
+  vpc_id = "vpc-02e09d5fbf4b98211"
+  subnets = ["subnet-04676ca5151426d2b", "subnet-053b91915bbcffd18"]
+  access_logs_bucket = module.s3_lb_log.s3_bucket_id
+  # certificate_arn    = "${module.certificate.acm_certificate_arn}"
 
-  enable_https_listener                  = true
+  enable_https_listener                  = false
   enable_http_listener                   = true
-  enable_redirect_http_to_https_listener = true
+  enable_redirect_http_to_https_listener = false
 
   internal                    = false
   idle_timeout                = 120
   enable_http2                = false
   ip_address_type             = "ipv4"
   access_logs_prefix          = "test"
-  access_logs_enabled         = true
+  access_logs_enabled         = false
   ssl_policy                  = "ELBSecurityPolicy-2016-08"
   https_port                  = 443
   http_port                   = 80
@@ -51,49 +57,49 @@ module "alb" {
   enable_deletion_protection = false
 }
 
-module "certificate" {
-  source      = "git::https://github.com/tmknom/terraform-aws-acm-certificate.git"
-  domain_name = "alb.${local.domain_name}"
-  zone_id     = "${data.aws_route53_zone.default.id}"
-}
-
-module "vpc" {
-  source                    = "git::https://github.com/tmknom/terraform-aws-vpc.git?ref=tags/1.0.0"
-  cidr_block                = "${local.cidr_block}"
-  name                      = "alb"
-  public_subnet_cidr_blocks = ["${cidrsubnet(local.cidr_block, 8, 0)}", "${cidrsubnet(local.cidr_block, 8, 1)}"]
-  public_availability_zones = ["${data.aws_availability_zones.available.names}"]
-}
-
 module "s3_lb_log" {
-  source                = "git::https://github.com/tmknom/terraform-aws-s3-lb-log.git?ref=tags/1.0.0"
-  name                  = "s3-lb-log-alb-${data.aws_caller_identity.current.account_id}"
+  source                = "git::https://github.com/tmknom/terraform-aws-s3-lb-log.git"
+  name                  = "s3-lb-log-alb-${var.alb_name}-${data.aws_caller_identity.current.account_id}"
   logging_target_bucket = "${module.s3_access_log.s3_bucket_id}"
   force_destroy         = true
 }
 
 module "s3_access_log" {
-  source        = "git::https://github.com/tmknom/terraform-aws-s3-access-log.git?ref=tags/1.0.0"
-  name          = "s3-access-log-alb-${data.aws_caller_identity.current.account_id}"
+  source        = "git::https://github.com/tmknom/terraform-aws-s3-access-log.git"
+  name          = "s3-access-log-alb-${var.alb_name}-${data.aws_caller_identity.current.account_id}"
   force_destroy = true
-}
-
-data "aws_route53_zone" "default" {
-  name = "${local.domain_name}."
-}
-
-locals {
-  cidr_block          = "10.255.0.0/16"
-  domain_name         = "${var.domain_name != "" ? var.domain_name : local.default_domain_name}"
-  default_domain_name = "nellmedina.com"
-}
-
-variable "domain_name" {
-  default     = "nellmedina.com"
-  type        = string
-  description = "If TF_VAR_domain_name set in the environment variables, then use that value."
 }
 
 data "aws_caller_identity" "current" {}
 
 data "aws_availability_zones" "available" {}
+
+# module "vpc" {
+#   source                    = "git::https://github.com/tmknom/terraform-aws-vpc.git"
+#   cidr_block                = "${local.cidr_block}"
+#   name                      = "alb"
+#   public_subnet_cidr_blocks = ["${cidrsubnet(local.cidr_block, 8, 0)}", "${cidrsubnet(local.cidr_block, 8, 1)}"]
+#   public_availability_zones = data.aws_availability_zones.available.names
+# }
+
+# module "certificate" {
+#   source      = "git::https://github.com/tmknom/terraform-aws-acm-certificate.git"
+#   domain_name = "alb.${local.domain_name}"
+#   zone_id     = "${data.aws_route53_zone.default.id}"
+# }
+
+# data "aws_route53_zone" "default" {
+#   name = local.domain_name
+# }
+
+# locals {
+#   cidr_block          = "10.255.0.0/16"
+#   domain_name         = var.domain_name != "" ? var.domain_name : local.default_domain_name
+#   default_domain_name = "nellmedina.com"
+# }
+
+# variable "domain_name" {
+#   default     = ""
+#   type        = string
+#   description = "If TF_VAR_domain_name set in the environment variables, then use that value."
+# }
